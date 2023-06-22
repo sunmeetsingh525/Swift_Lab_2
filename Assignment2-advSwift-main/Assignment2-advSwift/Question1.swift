a99a90a50116f0720a63e1857eb94d69e3189fb5
anchalladhar <133622298+anchalladhar@users.noreply.github.com>
2023-06-21 21:33:59 -0400

    Update Question1.swift

--- Assignment2-advSwift-main/Assignment2-advSwift/Question1.swift
+++ Assignment2-advSwift-main/Assignment2-advSwift/Question1.swift
@@ -1,10 +1,3 @@
-//
-//  Question1.swift
-//  Assignment2-advSwift
-//
-//  Created by Cambrian on 2022-10-17.
-//
-
 import Foundation
 
 // Function 1: Calculate the area of a square
@@ -16,7 +9,7 @@ func calculateArea(height: Int, width: Int) -> Int {
 func allCap(string: String) -> String {
     return string.uppercased()
 }
- 
+
 // Function 3: Extract the middle element of an array
 func middle(array: [Int]) -> Int? {
     let count = array.count
@@ -33,19 +26,19 @@ func middle(array: [Int]) -> Int? {
 }
 
 let squareArea = calculateArea(height: 4, width: 2)
-print("Area of square is:", squareArea)
+print("Area of the square is:", squareArea)
 
 let uppercaseString = allCap(string: "hello swift")
 print("Uppercased string is:", uppercaseString)
 
-let array1 = [1, 2, 3, 4, 5, 6, 7] // case where array has middle element
-if let middleElement = middle(array: array1) { 
+let array1 = [1, 2, 3, 4, 5, 6, 7] // Case where the array has a middle element
+if let middleElement = middle(array: array1) {
     print("Middle element:", middleElement)
 } else {
     print("No middle element")
 }
 
-let array2 = [1, 2, 3, 4] // case where array has no middle element
+let array2 = [1, 2, 3, 4] // Case where the array has no middle element
 if let middleElement = middle(array: array2) {
     print("Middle element:", middleElement)
 } else {
