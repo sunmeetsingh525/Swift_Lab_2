66b1ae0db71162f90ea6501994006ad9274b30e6
Sunmeet Singh <sunmeetsingh257@gmail.com>
2023-06-21 20:59:31 -0400

    Quest 1 solved

--- Assignment2-advSwift-main/Assignment2-advSwift/Question1.swift
+++ Assignment2-advSwift-main/Assignment2-advSwift/Question1.swift
@@ -6,39 +6,48 @@
 //
 
 import Foundation
-/**
- Question 1:
- for this questions you need to create 3 functions
- read the questions carefully - make you the function names parameters and return type are all the same as the question
+
+// Function 1: Calculate the area of a square
+func calculateArea(height: Int, width: Int) -> Int {
+    return height * width
+}
+
+// Function 2: Convert a string to all caps
+func allCap(string: String) -> String {
+    return string.uppercased()
+}
  
- FUNCTION 1:
- create a function that will calculate the area of a square
- function name:
-    - calculateArea
- parameters:
-    - height: Int
-    - width: Int
- return type:
-    - Int
- 
- FUNCTION 2:
- create a function that will convert and print a string into all caps
- function name:
-    - allCap
- parameters:
-    - string: String
- return type:
-    - String
- 
- FUNCTION 3:
- create a function that will return the middle element of an array
- function name:
-    - middle
- paramters:
-    - array: [Int]
- return type
-    - Int?
- NOTE, if there is no middle element return nil. there is no middle element if the value is even
- ex: [1,2,3,4,5,6] has no middle because 3,4 are both in the middle
- ex: [1,2,3,4,5] has a middle of 3
- */
+// Function 3: Extract the middle element of an array
+func middle(array: [Int]) -> Int? {
+    let count = array.count
+    guard count > 0 else {
+        return nil
+    }
+    
+    if count % 2 == 0 {
+        return nil
+    } else {
+        let middleIndex = count / 2
+        return array[middleIndex]
+    }
+}
+
+let squareArea = calculateArea(height: 4, width: 2)
+print("Area of square is:", squareArea)
+
+let uppercaseString = allCap(string: "hello swift")
+print("Uppercased string is:", uppercaseString)
+
+let array1 = [1, 2, 3, 4, 5, 6, 7] // case where array has middle element
+if let middleElement = middle(array: array1) { 
+    print("Middle element:", middleElement)
+} else {
+    print("No middle element")
+}
+
+let array2 = [1, 2, 3, 4] // case where array has no middle element
+if let middleElement = middle(array: array2) {
+    print("Middle element:", middleElement)
+} else {
+    print("No middle element")
+}
